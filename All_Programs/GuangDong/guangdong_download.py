dc2de5948ce1737c11f0586c76ac247c7d9048cb
weichen12345 <674559360@qq.com>
2018-12-26 17:14:28 +0800

    广东爬虫更新

--- All_Programs/GuangDong/guangdong_download.py
+++ All_Programs/GuangDong/guangdong_download.py
@@ -104,29 +104,33 @@ class GuangDong(TaxConfig):
         self.session = requests.session()
         captcha = self.recognition_img()
         # for p in range(3840,31454):
-        for p in range(page,31454):
-            if p % 10 == 0:
-                print('page:',p)
-            last_update_time = time.strftime('%Y-%m-%d %H:%M:%S')
-            trs = self.get_tags(captcha,p)
-            for tr in trs:
-                tds = tr.find_all('td')
-                # print(tds[0].find('a').get('href'))
-                # print(tds)
-                nsrmc = tds[0].text.strip()
-                nsrsbh = tds[1].text.strip()
-                fddbr = tds[2].text.strip()
-                qsje = tds[3].text.strip()
-                fbrq = tds[4].text.strip()[:4] + '-07-01'
-                # for td in tds:
-                #     print(td.text)
-                sql = "insert into taxplayer_qsgg (province,nsrmc,nsrsbh,fddbr,qsje,fbrq,last_update_time) values ('%s','%s','%s','%s','%s','%s','%s')" % (self.province,nsrmc,nsrsbh,fddbr,qsje,fbrq,last_update_time)
-                # print(sql)
-                # print(sql)
-                self.save_to_mysql(sql,log_name=self.log_name)
-                # print(tr)
-                # time.sleep(1000)
-
+        p = 1
+        try:
+            for p in range(page,31454):
+                if p % 10 == 0:
+                    print('page:',p)
+                last_update_time = time.strftime('%Y-%m-%d %H:%M:%S')
+                trs = self.get_tags(captcha,p)
+                for tr in trs:
+                    tds = tr.find_all('td')
+                    # print(tds[0].find('a').get('href'))
+                    # print(tds)
+                    nsrmc = tds[0].text.strip()
+                    nsrsbh = tds[1].text.strip()
+                    fddbr = tds[2].text.strip()
+                    qsje = tds[3].text.strip()
+                    fbrq = tds[4].text.strip()[:4] + '-07-01'
+                    # for td in tds:
+                    #     print(td.text)
+                    sql = "insert into taxplayer_qsgg (province,nsrmc,nsrsbh,fddbr,qsje,fbrq,last_update_time) values ('%s','%s','%s','%s','%s','%s','%s')" % (self.province,nsrmc,nsrsbh,fddbr,qsje,fbrq,last_update_time)
+                    # print(sql)
+                    # print(sql)
+                    self.save_to_mysql(sql,log_name=self.log_name)
+                    # print(tr)
+                    # time.sleep(1000)
+        except Exception as e:
+            print(e)
+            self.qs_province(p)
 
 
 if __name__ == '__main__':
